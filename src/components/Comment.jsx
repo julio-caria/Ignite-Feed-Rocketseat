dcfe74a1ead7df4cfad6adecc28f85944497a9db
Júlio Caria <juliocvc2004@hotmail.com>
2023-02-27 12:33:58 -0300

    Ignite Feed - RocketSeat

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return(
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="27 de Fevereiro às 11:21h" dateTime="2023-02-27 11:21:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, Parabéns! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
